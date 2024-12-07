d725abda55ab20354c44ce25bba3b54424c294d4
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2024-12-07 22:49:16 +0800

    測試環境變數

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/TheWelcome.vue';
+
+const apiRoot = import.meta.env.VITE_API_ROOT;
+const apiProjectPath = import.meta.env.VITE_API_PROJECT_PATH;
 </script>
 
 <template>
   <main>
+    <p>API Root：{{ apiRoot }}</p>
+    <p>API Project Path：{{ apiProjectPath }}</p>
+
+    <div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+    </div>
+
     <TheWelcome />
   </main>
 </template>
